a81f4619175777569de37d73ec7cb9ff629f65db
henock <henoctumonakiese@gmail.com>
2026-01-19 15:43:04 +0100

    creation de ghost builder

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+   namespace App\controllers;
+
+  class HomeController  extends Controller
+  {
+
+    public function index()
+    {
+        echo "home";
+    }
+  }
+
+?>
---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+  use App\controllers\HomeController;
+  use Router\Router;
+
+  Router::get('/',[HomeController::class,'index']);
+?>
