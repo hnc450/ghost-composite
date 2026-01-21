d2a7bc27e3672c089c109375a8522fac71a0e9de
henock <henoctumonakiese@gmail.com>
2026-01-21 15:31:21 +0100

    amelioration de ghost v 1.0.1

--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@
 
    require dirname(__DIR__). DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-
    require dirname(__DIR__). DIRECTORY_SEPARATOR . 'routes' . DIRECTORY_SEPARATOR . 'web.php';
    require dirname(__DIR__). DIRECTORY_SEPARATOR . 'routes' . DIRECTORY_SEPARATOR . 'api.php';
    
