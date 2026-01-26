86c47997bb9f810e304e59e5f865b65ea0a96808
henock <henoctumonakiese@gmail.com>
2026-01-26 01:58:13 +0100

    feat(view-engine): ajout du moteur de template dynamique

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,10 @@
   use Router\Router;
 
   Router::get('/',[HomeController::class,'index']);
+
+  Router::get('/test',function(){
+    $test = new \Tests\ViewTest();
+    $test->testViewRendersTemplate();
+  });
+  
 ?>
