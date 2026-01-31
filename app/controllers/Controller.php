67f7b21eff302c44a9ec7566c59ce83caae0c993
hnc450 <henoctumonakiese@gmail.com>
2026-01-31 11:50:53 +0100

    feat(controller):Implement singleton pattern and add error handling

--- app/controllers/Controller.php
+++ app/controllers/Controller.php
@@ -2,9 +2,34 @@
   namespace App\controllers;
   class Controller
   {
+    private static $controller;
+    
+    private static function instance():self{
+        if(is_null(self::$controller))
+        {
+          self::$controller = new Controller();
+        }
+        return self::$controller;
+    }
+    
     protected function sanitaze(string $input):string
     {
        return strip_tags(htmlspecialchars($input));
+    }
+     public static function error(int $error)
+    {
+      \http_response_code($error);
+     return self::instance();
+    }
+    
+    public static function json($array)
+    {
+      echo json_encode($array,JSON_PRETTY_PRINT);
+    }
+
+    public function inputs(){
+      $datas = \file_get_contents('php://input');
+      return $datas;
     }
   }
 ?>
